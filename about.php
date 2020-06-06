ef732301ebfdc829368500c24d272eae101ff5b6
Putri Isna Muliyani AR <putri_isna@mhs.unsyiah.ac.id>
2020-06-06 19:27:00 +0700

    /FinalSDA

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta charset="utf-8">
+                <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+                    <link href="assets\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+                        <link href="assets\css\fontawesome-free\css\all.min.css" rel="stylesheet">
+                            <link href="assets\css\style.css" rel="stylesheet">
+                                <title>
+                                    Profil Saya
+                                </title>
+                            </link>
+                        </link>
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body style="background-color: tan">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg"  style="background-color: RosyBrown">
+            <a style="color: white; font-family: 'marck script',cursive;" class="navbar-brand font-weight-bold" href="index.php">
+                <i class="fas fa-edit">
+                </i>
+                DATA EDITOR
+            </a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a  style="color: tan; font-family: 'marck script',cursive;" class="nav-link" href="cari.php">
+                            CARI EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a  style="color: tan; font-family: 'marck script',cursive;" class="nav-link" href="tampil.php">
+                            NAMA EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a   style="color: white; font-family: 'marck script',cursive;" class="nav-link" href="about.php">
+                            ABOUT ME
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="container mt-5">
+        <center>
+            <p>
+               <b style ="font-family: 'marck script',cursive; color:white">ASSALAMUALAIKUM!!</b> <br>
+               Nama :Putri Isna Muliyani AR<br> 
+               TTL : 20 Juni 2001<br>
+               Status : Mahasiswi<br>
+               Jurusan :  S1 Informatika Universitas Syiah Kuala
+               <br>
+            </p>
+            <img src="assets\\img\\foto.jpg" width="20%">
+            </center>
+        </div>
+        <script src="assets\bootstrap\js\jquery.min.js">
+        </script>
+        <script src="assets\bootstrap\js\bootstrap.min.js">
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\css\fontawesome-free\css\all.min.css">
+    <link rel="stylesheet" href="assets\css\style.css">
+	<title>Data Editor</title>
+</head>
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg " style="background-color: RosyBrown">
+	  <a style="color: white; font-family: 'marck script',cursive;" class="navbar-brand font-weight-bold" href="#"><i class="fas fa-edit"></i> DATA EDITOR</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item active">
+	        <a style="color: white; font-family: 'marck script',cursive;" class="nav-link" href="cari.php">CARI EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a style="color: white; font-family: 'marck script',cursive;" class="nav-link" href="tampil.php">NAMA EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a style="color: white; font-family: 'marck script',cursive;"  class="nav-link" href="about.php">ABOUT ME</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	<!-- End Navbar -->
+
+	<!-- Jumbotron -->
+	<div class="jumbotron mb-0">
+	  <h1 class="display-4" style ="font-family: 'marck script',cursive">Hello!</h1>
+	  <p class="lead" style ="font-family: 'marck script',cursive;color : rosybrown" > Website ini adalah website yang akan menampilkan informasi editor pada artikel situs detik.com beserta artikel yang telah diedit<br>Data yang diperoleh berasal dari struktur data hash yang diintegrasi dengan Web</p>
+	  <hr class="my-4" style ="font-family: 'marck script',cursive">
+	  <p style ="font-family: 'marck script',cursive;color : rosybrown">Website ini dibuat untuk memenuhi tugas Projek Lab Matakuliah Struktur Data dan Algoritma Program Studi Informatika Unsyiah</p>
+	  <a class="btn btn-lg" href="https://github.com/putriisnaaaa/FinalSDA" role="button " style ="background-color:rosybrown"><i class="fab fa-github" style ="font-family: 'marck script',cursive;"></i> Ayo Kunjungi githubku <3 </a>
+	</div>
+	<!-- End Jumbotron -->
+
+	<script src="assets\bootstrap\js\jquery.min.js"></script>
+    <script src="assets\bootstrap\js\bootstrap.min.js"></script>
+
+</body>
+</html>
+
